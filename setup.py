d0ab9e5297b8841d337b8fbb4c8b0f231aedab43
ivm <stan777.durand777@gmail.com>
2024-08-28 10:41:26 +0200

    update folder for cmake and setup to compile in docker

--- setup.py
+++ setup.py
@@ -14,13 +14,27 @@ setup(
             sources=['dpvo/altcorr/correlation.cpp', 'dpvo/altcorr/correlation_kernel.cu'],
             extra_compile_args={
                 'cxx':  ['-O3'], 
-                'nvcc': ['-O3'],
+                'nvcc': ['-O3',
+                    '-gencode=arch=compute_60,code=sm_60',
+                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_70,code=sm_70',
+                    '-gencode=arch=compute_75,code=sm_75',
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',
+                ]
             }),
         CUDAExtension('cuda_ba',
             sources=['dpvo/fastba/ba.cpp', 'dpvo/fastba/ba_cuda.cu', 'dpvo/fastba/block_e.cu'],
             extra_compile_args={
                 'cxx':  ['-O3'], 
-                'nvcc': ['-O3'],
+                'nvcc': ['-O3',
+                    '-gencode=arch=compute_60,code=sm_60',
+                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_70,code=sm_70',
+                    '-gencode=arch=compute_75,code=sm_75',
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',
+                ]
             },
             include_dirs=[
                 osp.join(ROOT, 'thirdparty/eigen-3.4.0')]
@@ -29,11 +43,20 @@ setup(
             include_dirs=[
                 osp.join(ROOT, 'dpvo/lietorch/include'), 
                 osp.join(ROOT, 'thirdparty/eigen-3.4.0')],
-            sources=[
-                'dpvo/lietorch/src/lietorch.cpp', 
-                'dpvo/lietorch/src/lietorch_gpu.cu',
-                'dpvo/lietorch/src/lietorch_cpu.cpp'],
-            extra_compile_args={'cxx': ['-O3'], 'nvcc': ['-O3'],}),
+                      sources=[
+                          'dpvo/lietorch/src/lietorch.cpp', 
+                          'dpvo/lietorch/src/lietorch_gpu.cu',
+                          'dpvo/lietorch/src/lietorch_cpu.cpp'],
+                      extra_compile_args={'cxx': ['-O3'], 
+                                          'nvcc': ['-O3',
+                                                   '-gencode=arch=compute_60,code=sm_60',
+                                                   '-gencode=arch=compute_61,code=sm_61',
+                                                   '-gencode=arch=compute_70,code=sm_70',
+                                                   '-gencode=arch=compute_75,code=sm_75',
+                                                   '-gencode=arch=compute_80,code=sm_80',
+                                                   '-gencode=arch=compute_86,code=sm_86',
+                                                   ]
+                                          }),
     ],
     cmdclass={
         'build_ext': BuildExtension
